dd4a103a7b4c9be20b1a2c2d46c1d48bddae12c6
Aymene <boughanmiaymene@gmail.com>
2026-05-16 17:24:12 +0000

    write user registration logic

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 import dbConnection from "./db/index.js";
 dbConnection();
-import authRoutes from "routes/auth.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 const app = express();
 const port = process.env.PORT;
-
+app.use(express.json());
+app.use("/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("test");
 });
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register } from "../controllers/user.controller.js";
+import { register } from "../controllers/auth.controller.js";
 import { userValidator } from "../validators/user.validator.js";
 
 const router = express.Router();
